f585542b1c09fcb59d17c33223b5d40a009aeaa5
harshadev-7 <2024cse.r102@svce.edu.in>
2025-10-28 21:19:21 +0530

    After adding image to a Jbutton

--- AWT and Swing/Swing Componets/JButton in Swing/JButtonExample.java
+++ AWT and Swing/Swing Componets/JButton in Swing/JButtonExample.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
+import java.awt.Image;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -10,13 +12,23 @@ public class JButtonExample extends JFrame implements ActionListener
     {
        setTitle("Basic Jbutton example in swings");
        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       setSize(250,250);
+       setSize(500,500);
+       setLayout(null);
        setVisible(true);
+       ImageIcon icon = new ImageIcon("C:\\Users\\harsh\\Desktop\\GUI\\GUI-java\\AWT and Swing\\Swing Componets\\JLabel in Swing\\goku.png");
        button = new JButton("Click Me");
-       button.setBounds(0, 0, 250, 250);
+       Image img = icon.getImage();
+       Image scaledImage = img.getScaledInstance(50,50,Image.SCALE_SMOOTH);
+       ImageIcon scaledIcon = new ImageIcon(scaledImage);
+       button.setBounds(200, 100, 150, 90);
+       button.setHorizontalTextPosition(JButton.CENTER);
+       button.setVerticalTextPosition(JButton.BOTTOM);
+       button.setHorizontalAlignment(JButton.CENTER);
+       button.setVerticalAlignment(JButton.CENTER);
        button.addActionListener(this);
+       button.setIcon(scaledIcon);
        button.setForeground(Color.CYAN);
-       button.setBackground(Color.BLACK);
+       button.setBackground(Color.DARK_GRAY);
        button.setFocusable(false);
        add(button);
     }
